de13e4a2ec637c586e46509bbd42d388fd7d2774
geusan <dnay2k@gmail.com>
2020-07-18 15:18:16 +0900

    add  routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
+app.use(bodyParser.json());
 
+app.use(routes)
 
 export { app };
