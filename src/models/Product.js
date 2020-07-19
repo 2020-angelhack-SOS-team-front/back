c027b36e39e5df13da625f98bb94f94c02ce9209
geusan <dnay2k@gmail.com>
2020-07-19 17:30:49 +0900

    제품 타입 추가

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,8 +1,23 @@
 import mongoose from 'mongoose';
 import { StoreSchema } from './Store';
 
+const ProductType = {
+  Vegetable: 'vegetable', // 채소
+  FruitNutRice: 'fruit_nut_rice', // 과일 견과 쌀
+  Seafood: 'seafood', // 수산 해선 건어물
+  MeatEgg: 'meat_egg', // 정육 계란
+  SoupSideMain: 'soup_side_main', // 국 반찬 메인
+  MyeonSpiceOil: 'myeon_spice_oil', // 면 양념 오일
+  BeverageSnack: 'beverageSnack', // 음료 우유 떡 간식
+  Lifestyle: 'lifestyle', // 생활용품
+  Special: 'special', // 특산품
+  Beauty: 'beauty', //뷰티 바디케어
+}
+Object.freeze(ProductType);
+
 const ProductSchema = mongoose.Schema({
   whatStore: StoreSchema,
+  type: String, // ProductType이 들어가야함
   name: String,
   price: Number,
   image: String,
@@ -12,4 +27,4 @@ const ProductSchema = mongoose.Schema({
 
 const Product = mongoose.model("Product", ProductSchema);
 
-export { Product, ProductSchema };
+export { Product, ProductSchema, ProductType };
