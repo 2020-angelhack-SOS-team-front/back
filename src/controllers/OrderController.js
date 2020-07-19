550a39d639355fb72fb30f42cab30f14ac5e8b4a
geusan <dnay2k@gmail.com>
2020-07-19 20:36:19 +0900

    fix: 주문하기 오류 수정

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -11,21 +11,22 @@ const transform = (order) => ({
 
 const transforms = (orders) => orders.map(transform);
 
-export const createOrder = async (req, res) => {
+export const createOrder = async (req, res, next) => {
   const products = await Product.find({ _id: { $in: req.body.items.map((item) => item.product_id) }}).exec();
   const productsByKey = products.reduce((keyBy, product) => ({ ...keyBy, [product._id]: product }), {});
   const order = new Order({
     name: req.body.name,
     address: req.body.address,
     items: req.body.items.map((item) => ({
-      product: item._id,
+      product: item.product_id,
       count: item.count,
     })),
     amount: req.body.items.reduce((sum, item) => sum + productsByKey[item.product_id].price * Number(item.count), 0),
   });
   const data = await order.save();
+  
   res.json({
-    data: transform(data),
+    data: transform(await Order.findById(data._id).populate('items.product').exec()),
   })
 };
 
@@ -38,8 +39,10 @@ export const indexOrders = async (req, res) => {
 }
 
 export const findOrder = async (req, res) => {
-  const data = await Order.findById(req.params.orderId).exec();
+  const order = await Order.findById(req.params.orderId).populate('items.product').exec();
   res.json({
-    data: transform(data),
+    data: transform(order),
   });
 }
+
+
